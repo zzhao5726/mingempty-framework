3908c45da64e4fd7ff3c16f099ce24529af2dff3
zzhao <945514650@qq.com>
2024-07-02 23:13:07 +0800

    cloud功能新增 1.配置项增强 2.增加condition 3.增加web工具类

---
+++ mingempty-cloud-spring-boot3-starter/src/main/java/top/mingempty/cloud/condition/ConditionalOnHavingProperties.java
@@ -0,0 +1,44 @@
+package top.mingempty.cloud.condition;
+
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * 当配置文件问存在配置时，则注入
+ * @author zzhao
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Conditional(HavingPropertiesCondition.class)
+public @interface ConditionalOnHavingProperties {
+
+    /**
+     * 前缀
+     *
+     */
+    String prefix() default "";
+
+
+    /**
+     * 值
+     * 只有都存在是才会注入
+     *
+     */
+    String[] value() default {};
+
+
+    /**
+     * 若不存在时是否依旧注入
+     *
+     */
+    boolean matchIfMissing() default false;
+
+}
---
+++ mingempty-cloud-spring-boot3-starter/src/main/java/top/mingempty/cloud/postprocessor/ApplicationEnvironmentPostProcessor.java
@@ -0,0 +1,60 @@
+package top.mingempty.cloud.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import top.mingempty.commons.util.StringUtil;
+
+/**
+ * 系统基础信息增强
+ *
+ * @author zzhao
+ */
+@Slf4j
+public class ApplicationEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+
+    /**
+     * Post-process the given {@code environment}.
+     *
+     * @param environment the environment to post-process
+     * @param application the application to which the environment belongs
+     */
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String appName = StringUtil.null2Str(StringUtil
+                .null2Str(environment.getProperty("me.name"),
+                        environment.getProperty("spring.application.name")), "mingempty");
+        String appGroup = StringUtil.null2Str(StringUtil
+                .null2Str(environment.getProperty("me.group"),
+                        environment.getProperty("spring.application.group")), "g");
+        String appVersion = StringUtil.null2Str(StringUtil
+                .null2Str(environment.getProperty("me.version"),
+                        environment.getProperty("spring.application.version")), "v");
+        environment.getSystemProperties().put("me.name", appName);
+        environment.getSystemProperties().put("spring.application.name", appName);
+        environment.getSystemProperties().put("me.group", appGroup);
+        environment.getSystemProperties().put("spring.application.group", appGroup);
+        environment.getSystemProperties().put("me.version", appVersion);
+        environment.getSystemProperties().put("spring.application.version", appVersion);
+        log.debug("系统基础信息增强。appName:[{}],appGroup:[{}],appVersion:[{}]", appName, appGroup, appVersion);
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE + 12;
+    }
+}
