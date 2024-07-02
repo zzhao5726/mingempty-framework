8b4c56b919d72300e85069c786c35b82c24b2bfd
zzhao <945514650@qq.com>
2024-07-02 23:13:07 +0800

    cloud功能新增 1.配置项增强 2.增加condition 3.增加web工具类

--- mingempty-cloud-spring-boot3-starter/src/main/java/top/mingempty/cloud/config/CloudConfiguration.java
+++ mingempty-cloud-spring-boot3-starter/src/main/java/top/mingempty/cloud/config/CloudConfiguration.java
@@ -2,7 +2,6 @@ package top.mingempty.cloud.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import top.mingempty.cloud.util.BeanFactoryUtil;
@@ -15,8 +14,7 @@ import top.mingempty.cloud.util.SpringContextUtil;
  *
  * @author zzhao
  */
-@Configuration
-public class MeCloudConfiguration {
+public class CloudConfiguration implements Ordered {
 
     @Bean
     @ConditionalOnMissingBean(name = "springContextUtil")
@@ -41,5 +39,22 @@ public class MeCloudConfiguration {
     @ConditionalOnMissingBean(name = "beanFactoryUtil")
     public static BeanFactoryUtil beanFactoryUtil() {
         return new BeanFactoryUtil();
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
