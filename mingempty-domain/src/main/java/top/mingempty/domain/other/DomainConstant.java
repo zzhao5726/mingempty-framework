8679da2c7c07af81bae73f3dd28abe7e1caa0c51
zzhao <945514650@qq.com>
2024-07-07 15:30:03 +0800

    链路功能调整

---
+++ mingempty-domain/src/main/java/top/mingempty/domain/other/DomainConstant.java
@@ -0,0 +1,26 @@
+package top.mingempty.domain.other;
+
+/**
+ * 静态常量类
+ *
+ * @author zzhao
+ */
+public interface DomainConstant {
+
+
+    /**
+     * 字符串常量：{@code "null"} <br>
+     * 注意：{@code "null" != null}
+     */
+    String NULL = "null";
+
+    /**
+     * 字符串常量：空字符串 {@code ""}
+     */
+    String EMPTY = "";
+
+    /**
+     * 字符串常量：空格符 {@code " "}
+     */
+    String SPACE = " ";
+}
