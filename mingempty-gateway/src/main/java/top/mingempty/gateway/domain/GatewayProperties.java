af05affa47ee8e7790d8f9fba8b19611ffc525b4
zzhao <945514650@qq.com>
2024-07-14 21:52:20 +0800

    集成gatewaay

---
+++ mingempty-gateway/src/main/java/top/mingempty/gateway/domain/GatewayProperties.java
@@ -0,0 +1,38 @@
+package top.mingempty.gateway.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 网关配置文件
+ *
+ * @author zzhao
+ */
+@Data
+@ConfigurationProperties(prefix = "me.gateway")
+@Schema(title = "网关配置文件", description = "网关配置文件")
+public class GatewayProperties {
+
+
+    /**
+     * 配置需要清洗的请求头
+     * <p>
+     * 内置会清理的header有：</br>
+     * <p>
+     * {@code request-from-inner}
+     */
+    @Schema(title = "配置需要清洗的请求头")
+    private List<String> clearHeaders = Collections.emptyList();
+
+    /**
+     * 跨域配置
+     */
+    @Schema(title = "跨域配置")
+    private CorsConfiguration corsConfiguration;
+
+}
