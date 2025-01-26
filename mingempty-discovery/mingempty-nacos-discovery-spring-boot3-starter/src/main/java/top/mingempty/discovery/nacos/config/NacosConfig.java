592dfd360d6967d6353e58070be4468551b3bc9f
zzhao <945514650@qq.com>
2025-01-26 17:42:52 +0800

    增加对nacos的封装

---
+++ mingempty-discovery/mingempty-nacos-discovery-spring-boot3-starter/src/main/java/top/mingempty/discovery/nacos/config/NacosConfig.java
@@ -0,0 +1,42 @@
+package top.mingempty.discovery.nacos.config;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.discovery.NacosDiscoveryAutoConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import top.mingempty.commons.util.DateTimeUtil;
+import top.mingempty.domain.MeGloableProperty;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+/**
+ * nacos自定义配置文件
+ */
+@Slf4j
+@Configuration
+@AllArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@AutoConfigureBefore(NacosDiscoveryAutoConfiguration.class)
+public class NacosConfig {
+
+    private final MeGloableProperty meGloableProperty;
+
+    @Bean
+    public NacosDiscoveryProperties nacosProperties() {
+        NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
+        Map<String, String> metadata = nacosDiscoveryProperties.getMetadata();
+        metadata.put("startup.time", DateTimeUtil.formatLocalDateTime(LocalDateTime.now()));
+        metadata.put("me.group", meGloableProperty.getGroup());
+        metadata.put("me.version", meGloableProperty.getVersion());
+        return nacosDiscoveryProperties;
+    }
+
+
+}
