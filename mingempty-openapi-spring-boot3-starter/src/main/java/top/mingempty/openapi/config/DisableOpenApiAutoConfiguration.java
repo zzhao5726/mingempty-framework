083959f65188fcc1324bb64ff7074eeef7d6d222
zzhao <945514650@qq.com>
2025-02-12 17:32:03 +0800

    springdoc调整

--- mingempty-openapi-spring-boot3-starter/src/main/java/top/mingempty/openapi/config/DisableOpenApiAutoConfiguration.java
+++ mingempty-openapi-spring-boot3-starter/src/main/java/top/mingempty/openapi/config/DisableOpenApiAutoConfiguration.java
@@ -1,18 +1,24 @@
 package top.mingempty.openapi.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 /**
  * springdoc默认配置
  *
  * @author zzhao
  */
-@Configuration
-@EnableAutoConfiguration(excludeName = {"org.springdoc.webflux.ui.SwaggerConfig",
-        "org.springdoc.webflux.ui.SwaggerConfig", "org.springdoc.core.configuration.SpringDocConfiguration"})
+@Order(10)
+@ConditionalOnMissingBean(value = {OpenApiAutoConfiguration.class})
+@EnableAutoConfiguration(excludeName = {"org.springdoc.webmvc.ui.SwaggerConfig",
+        "org.springdoc.webflux.ui.SwaggerConfig", "org.springdoc.core.configuration.SpringDocConfiguration",
+        "org.springdoc.webmvc.core.configuration.MultipleOpenApiSupportConfiguration"})
 @ConditionalOnProperty(prefix = "me.openapi", name = "enabled", havingValue = "false", matchIfMissing = true)
 public class DisableOpenApiAutoConfiguration {
 
+    public DisableOpenApiAutoConfiguration() {
+        System.out.println("");
+    }
 }
