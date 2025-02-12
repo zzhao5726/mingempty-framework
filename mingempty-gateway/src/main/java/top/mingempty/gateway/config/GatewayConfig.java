a93a5f616b68157610fc9d5e7c37a551fc6af71d
zzhao <945514650@qq.com>
2025-02-12 23:56:53 +0800

    调整配置 调整跨域

--- mingempty-gateway/src/main/java/top/mingempty/gateway/config/GatewayConfig.java
+++ mingempty-gateway/src/main/java/top/mingempty/gateway/config/GatewayConfig.java
@@ -4,11 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
-import top.mingempty.gateway.domain.GatewayConstant;
 import top.mingempty.gateway.domain.GatewayProperties;
 import top.mingempty.gateway.filter.MeGlobakFilter;
 
@@ -34,37 +29,4 @@ public class GatewayConfig {
         return new MeGlobakFilter(gatewayProperties);
     }
 
-
-    /**
-     * 跨域问题处理
-     *
-     * @return 过滤器
-     */
-    @Bean
-    public CorsWebFilter corsWebFilter(GatewayProperties gatewayProperties) {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-        source.registerCorsConfiguration("/**", corsConfiguration(gatewayProperties));
-        return new CorsWebFilter(source);
-    }
-
-
-    private CorsConfiguration corsConfiguration(GatewayProperties gatewayProperties) {
-        CorsConfiguration corsConfiguration = gatewayProperties.getCorsConfiguration();
-        if (corsConfiguration != null) {
-            return corsConfiguration;
-        }
-        corsConfiguration = new CorsConfiguration();
-        // 允许cookies跨域
-        corsConfiguration.setAllowCredentials(true);
-        // #允许向该服务器提交请求的URI，*表示全部允许
-        corsConfiguration.addAllowedOriginPattern(GatewayConstant.ALL);
-        // #允许访问的头信息,*表示全部
-        corsConfiguration.addAllowedHeader(GatewayConstant.ALL);
-        // 允许提交请求的方法类型，*表示全部允许
-        corsConfiguration.addAllowedMethod(GatewayConstant.ALL);
-        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-        corsConfiguration.setMaxAge(GatewayConstant.MAX_AGE);
-
-        return corsConfiguration;
-    }
 }
